3221259bd4ae3afc8fb106d472b6639279864d3a
ankitabash <niitz.man@gmail.com>
2017-10-27 13:00:01 -0400

    nitika

---
+++ neimai/src/test/java/nitika/neimai/demo.java
@@ -0,0 +1,12 @@
+package nitika.neimai;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	@Test
+	public void testing()
+	{
+		System.out.println("hello");
+	}
+
+}
---
+++ neimai/src/test/java/nitika/neimai/pleasure.java
@@ -0,0 +1,13 @@
+package nitika.neimai;
+
+import org.testng.annotations.Test;
+
+public class pleasure {
+	@Test
+	public void casse()
+	{
+		System.out.println("hi from java");
+	}
+	
+
+}
